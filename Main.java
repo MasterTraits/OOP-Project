702c89ac051b5ae939122d08614d76dd5fcadcb9
Emmanuel Fabella <efrohibire@gmail.com>
2024-10-12 14:49:47 +0000

    Bring back everything

---
+++ Main.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
+class Authentication {
+  public Scanner sc = new Scanner(System.in);
+  private String username, password;
+
+  public void getUsername() {
+    System.out.println("Enter your username: ");
+    String username = sc.nextLine();
+  }
+  // Di pa tapos
+  // mahal kita arf arf
+}
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+}
