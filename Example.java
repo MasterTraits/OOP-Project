b425210ff06f00475ce71b5aab25097abc3b4a75
Emmanuel Fabella <efrohibire@gmail.com>
2024-10-12 11:37:47 +0800

    Main.java

---
+++ Example.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
+class Authentication {
+  public Scanner sc = new Scanner(System.in);
+  private String username, password;
+
+  public void getUsername() {
+    System.out.println("Enter your username: ");
+    String username = sc.nextLine();
+  }
+
+  // Di pa tapos ni Emman 
+  
+}
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+}
